8f4ff60b88c3a575ea993a88177e1a952e74eab8
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-28 04:28:46 +0500

    Render CreateNewComponent in Books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Book from './Book';
+import CreateNewBook from './CreateNewBook';
 
 function Books() {
   return (
     <section>
       <Book title="Zain's New Book" author="Zain Sadaqat" />
       <Book title="Zain's Old Book" author="Zain Sadaqat" />
+      <hr />
+      <CreateNewBook />
     </section>
   );
 }
--- src/components/CreateNewBook.js
+++ src/components/CreateNewBook.js
@@ -1,9 +1,13 @@
 function CreateNewBook() {
   return (
-    <form>
-      <input type="text" placeholder="Add a Book" />
-      <input type="text" placeholder="Add a Author" />
-    </form>
+    <>
+      <h2>Add Book Info</h2>
+      <form>
+        <input type="text" placeholder="Add a Book" />
+        <input type="text" placeholder="Add a Author" />
+        <input type="submit" />
+      </form>
+    </>
   );
 }
 
